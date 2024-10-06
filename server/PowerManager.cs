88b1de877a3c6dec215d2c264039fe20be4bfef4
Helloyunho <yunho050840@gmail.com>
2024-10-06 21:09:28 +0900

    🚸 feat: add more infos to process

--- server/PowerManager.cs
+++ server/PowerManager.cs
@@ -4,11 +4,11 @@ class PowerManager
 {
     public static void Shutdown()
     {
-        Windows.System.ShutdownManager.BeginShutdown(Windows.System.ShutdownKind.Shutdown, new TimeSpan(0));
+        ShutdownManager.BeginShutdown(ShutdownKind.Shutdown, new TimeSpan(0));
     }
 
     public static void Reboot()
     {
-        Windows.System.ShutdownManager.BeginShutdown(Windows.System.ShutdownKind.Restart, new TimeSpan(0));
+        ShutdownManager.BeginShutdown(ShutdownKind.Restart, new TimeSpan(0));
     }
 }
--- server/ProcessManager.cs
+++ server/ProcessManager.cs
@@ -4,11 +4,15 @@ class ProcessJson
 {
     public int Id { get; init; }
     public string Name { get; init; }
+    public long Memory { get; init; }
+    public bool Responding { get; init; }
 
     public ProcessJson(Process process)
     {
         Id = process.Id;
         Name = process.ProcessName;
+        Memory = process.WorkingSet64;
+        Responding = process.Responding;
     }
 }
 
