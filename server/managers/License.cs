54df344d8f4ac835963ed6fdddd8eb002b966670
Helloyunho <yunho050840@gmail.com>
2024-10-23 00:16:23 +0900

    ⚗️ fix: forgor to push this

--- server/managers/License.cs
+++ server/managers/License.cs
@@ -16,7 +16,7 @@ class LicenseManager
         {
             using (SqliteConnection connection = new SqliteConnection($"Data Source={LicenseDB}"))
             {
-                raw.SetProvider(new SQLite3Provider_sqlite3());
+                raw.SetProvider(new SQLite3Provider_winsqlite3());
                 connection.Open();
                 var files = Directory.GetFiles(LicenseDir, "*", SearchOption.TopDirectoryOnly);
                 foreach (string file in files)
